a11b67cd6b28b8caae077b8bda230926b79867b8
Anastasia <tsepelevaa@gmail.com>
2024-09-28 10:17:09 +0300

    feat: add logo and decor (Sat, Sep 28, 2024 10:17:10 AM)

--- image-gallery/index.js
+++ image-gallery/index.js
@@ -2,7 +2,6 @@
     let url = 'https://api.unsplash.com/search/photos?query=winter&per_page=18&orientation=landscape&client_id=UQu_pjTJlo_eDCQOjJ5Wao9POFiFc66pqG7jYYmXHl8';
     const input = document.querySelector('#search');
     const searchBtn = document.querySelector('#search-btn');
-    let isSearch = false;
 
     async function getImages() {
         const res = await fetch(url);
@@ -26,7 +25,7 @@
     getImages();
 
     function searchNewImages() {
-        const value = input.value || 'photos';
+        const value = input.value || 'winter';
         url = 'https://api.unsplash.com/search/photos?query='
                 + value
                 + '&per_page=18&orientation=landscape&client_id=UQu_pjTJlo_eDCQOjJ5Wao9POFiFc66pqG7jYYmXHl8';
@@ -35,24 +34,23 @@
         searchBtn.removeEventListener('click', searchNewImages);
         searchBtn.addEventListener('click', clearSearch);
         getImages();
+        input.focus();
     }
 
     function clearSearch() {
         input.value = '';
         toggleBtn();
-        isSearch = false;
+        input.focus();
     }
 
     function toggleBtn() {
-            searchBtn.classList.remove('clear');
-            searchBtn.classList.add('search');
-            searchBtn.removeEventListener('click', clearSearch);
-            searchBtn.addEventListener('click', searchNewImages);
-    // isSearch = false;
-        // }
+        searchBtn.classList.remove('clear');
+        searchBtn.classList.add('search');
+        searchBtn.removeEventListener('click', clearSearch);
+        searchBtn.addEventListener('click', searchNewImages);
     }
 
-    // input.addEventListener('change', searchNewImages);
+    input.focus();
     input.addEventListener('keypress', event => {
         if (event.key === 'Enter') {
             searchNewImages();
